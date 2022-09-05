37e37ed4f2ffb0a9f30922a3b599b7c7b8111e00
Roku-3 <down01134@gmail.com>
2022-09-05 13:37:14 +0900

    add scenes filter

--- Minge2022Summer/src/common.hpp
+++ Minge2022Summer/src/common.hpp
@@ -1,18 +1,8 @@
 ﻿#pragma once
 #include <Siv3D.hpp>
-//#define DEBUG
-class Tile;
 
 struct GameData {
 	bool isMapSelected = false;
-	FilePath selectedMap;
-
-	bool isMapSet = false;
-	Array<Array<Tile>> map;
-	Point startPoint;
-	Point goalPoint;
-	SecondsF playerTime = 60.0s;		// プレイヤーシーンの制限時間
-	SecondsF enemyTime = 25.0s;			// エネミーシーンの制限時間
 };
 
 using App = SceneManager<String, GameData>;
