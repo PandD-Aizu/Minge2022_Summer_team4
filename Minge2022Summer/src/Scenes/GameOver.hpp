c7f493404dd73bc4a4104f73ab1802f1f055707a
tsoushi <sosimds@gmail.com>
2022-10-29 03:43:51 +0900

    GameOverシーンに画像を追加

--- Minge2022Summer/src/Scenes/GameOver.hpp
+++ Minge2022Summer/src/Scenes/GameOver.hpp
@@ -5,10 +5,10 @@ class GameOver : public App::Scene {
 private:
 	Font gameClearFont{ 60 };
 	const Texture bg{ U"GameOverBG.png" };
-	const Rect continueBtn{ 64, 280, 512, 64 };
-	const Texture continueBtnTexture{ U"backToTitleBtn.png" };
-	const Rect titleBtn{ 64, 376, 512, 64 };
-	const Texture titleBtnTexture{ U"backToTitleBtn.png" };
+	const Rect continueBtn{ Arg::center(Scene::Width() / 2, Scene::Height() / 2 + 60), 224, 64 };
+	const Texture continueBtnTexture{ U"GameOver_restart.png" };
+	const Rect titleBtn{ Arg::center(Scene::Width() / 2, Scene::Height() / 2 + 150), 368, 64 };
+	const Texture titleBtnTexture{ U"GameOver_backToTitleBtn.png" };
 
 public:
 	GameOver(const InitData& init);
